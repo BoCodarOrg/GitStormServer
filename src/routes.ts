91a5d9e694dbaae944f7321ee031ac91f51c3fb3
Lucas <lucasjoao85@gmail.com>
2020-12-30 15:44:32 -0300

    Desenvolvendo diff dos pull requests

--- src/routes.ts
+++ src/routes.ts
@@ -1,36 +1,38 @@
 import express, { NextFunction, Request, Response } from 'express';
 
-import { spawn } from 'child_process';
+import { exec } from 'child_process';
 import parseToObject from './util/parseToObject';
 
 const router = express.Router();
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
-    const branch = spawn('git', ['branch']);
-    branch.stdout.on('data', (data: any) => {
-        branch.on('close', (code) => console.log('branch::', code));
-        return res.render('home', { data: data.toString().trim().split('\n') });
-
-    });
+    exec('git branch', (error, stdout, stderr) => {
+        return res.render('home', { data: stdout.toString().trim().split('\n') });
+    })
 })
 
 
 router.get('/branch/:branch', (req: Request, res: Response, next: NextFunction) => {
-    const commits = spawn('git', ['log', req.params.branch, '--pretty=format:{"commit":"%h","date":"%ad","message":"%s","author":"%an", "email":"%ce"}', '--date=short'])
-    commits.stdout.on('data', data => {
-        commits.on('close', (code) => console.log('commit::', code));
-        const result = parseToObject(`${data}`);
-        return res.render('commit', { data: result, branch: req.params.branch });
-    })
+    const cmd = `git log ${req.params.branch} --pretty=format:'{"commit":"%h","date":"%ad","message":"%s","author":"%an", "email":"%ce"}' --date=short`;
+    exec(cmd, (error, stdout, stderr) => {
+        if (!error) {
+            const result = parseToObject(`${stdout}`);
+            return res.render('commit', { data: result, branch: req.params.branch });
+        } else {
+            console.log('error::', error);
+            return res.send(`error::${error}`)
+        }
+
+    });
 });
 
 router.get('/pull-request/:branch', (req: Request, res: Response, next: NextFunction) => {
-    const log = spawn('git', ['log', req.params.branch ,'--pretty=format:%d']);
-    log.stdout.on('data', data => {
-        const branch = data.toString().split('\n')[0];
-        const parent = branch.split(',')[1].replace(')', '');
-        return res.render('pullRequest', { branch: req.params.branch, toBranch: parent });
-    });
+    const cmd = 'git branch';
+    exec(cmd, (error, stdout, stderr) => {
+        return res.render('pullRequest', {
+            branch: req.params.branch, branches: stdout.toString().trim().split('\n')
+        });
+    })
 })
 
 export default router;
--- src/views/pullRequest.tsx
+++ src/views/pullRequest.tsx
@@ -2,11 +2,37 @@ import React from 'react';
 
 interface Props {
     branch: string,
-    toBranch: string
+    branches: [string]
 }
 
 const PullRequest: React.FC<Props> = (props) => {
-    return <h1>{props.branch} to {props.toBranch}</h1>
+    return (
+        <div>
+            <h1>Pullrequest to {props.branch}</h1>
+            {props.branch} from {`  `}
+            <select>
+                {
+                    props.branches.map((item, index) => {
+                        return <option value={index}>{item}</option>
+                    })
+                }
+            </select>
+            <br />
+            <label>
+                Pull request title: {` `}
+                <input placeholder="Pull request title" type="text" name="title" />
+            </label>
+            <br />
+            <label>Pull Request Description</label>
+            <br />
+            <textarea style={{ width: 400, height: 150, resize: 'none' }}>
+                Describe your pull request...
+            </textarea>
+            <br />
+            <button>Create pull request</button>
+        </div>
+    )
+
 }
 
 export default PullRequest;
